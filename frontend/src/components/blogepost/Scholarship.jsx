61ad0abec7131d5d491934c8a34ad2d7e91f9d4d
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-04-28 08:46:54 -0700

    Update Scholarship.jsx

--- frontend/src/components/blogepost/Scholarship.jsx
+++ frontend/src/components/blogepost/Scholarship.jsx
@@ -8,7 +8,7 @@ const Scholarship = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/get_scholarship_post')
+      .get('https://frontendjcsone.vercel.app/get_scholarship_post')
       .then((res) => {
         setData(res.data.data); // Adjust according to API response structure
       })
