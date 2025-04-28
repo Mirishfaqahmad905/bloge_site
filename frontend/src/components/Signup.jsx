0ed28d3fa8de7d35099d6fc27b41e9b8f7fe0e5c
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-04-28 08:44:46 -0700

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -34,7 +34,7 @@ const Signup = () => {
 
     try {
       // Send data to the backend API
-      const response = await fetch('http://localhost:3000/api/signup', {
+      const response = await fetch('https://frontendjcsone.vercel.app/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
