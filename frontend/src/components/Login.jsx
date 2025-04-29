395f3dd249e3a9303851b0eb093beeb77de80824
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-04-28 21:04:41 -0700

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch(`https://frontendjcsone.vercel.app/api/login`, {
+      const response = await fetch(`https://bloge-backend.vercel.app/api/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
